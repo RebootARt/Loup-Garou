54f974dd091ad63d7b9cbd856834e5b914f0b8e7
Gab55 <gabrielgiroud@gmail.com>
2018-11-19 18:27:50 +0100

    Mass Update

--- LoupGarou/LoupGarou/Voleur.cs
+++ LoupGarou/LoupGarou/Voleur.cs
@@ -8,5 +8,14 @@ namespace LoupGarou
 {
     class Voleur
     {
+        private string nom;
+        private List<Villageois> listVoleur = new List<Villageois>();
+
+        public Voleur(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
     }
 }
